550647eb7321daaf381ccbf210be08e81f167ec4
matetiau <matekamer@gmail.com>
2018-07-16 16:16:50 +0300

    why

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
